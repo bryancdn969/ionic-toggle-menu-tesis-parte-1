a190f23188576f65d6eda5d2eaa6173aa2d08645
EPS-PC\Bryan <bryan.ocana@epn.edu.ec>
2019-01-20 22:12:31 -0500

    Continue profile

--- tesis-parte-1-menu/src/pages/profile/profile.ts
+++ tesis-parte-1-menu/src/pages/profile/profile.ts
@@ -1,12 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProfilePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import {Component, ViewChild} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {ListPage} from "../list/list";
 
 @IonicPage()
 @Component({
@@ -15,7 +10,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProfilePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('dateTime') dateTime;
+
+  form: FormGroup;
+  myDate: FormControl = new FormControl('', Validators.required);
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private fb: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      myDate: this.myDate
+    });
+
+    setTimeout(_ => {
+      this.dateTime.setValue(new Date().toISOString());
+    });
+  }
+
+  list(){
+    this.navCtrl.setRoot(ListPage);
   }
 
   ionViewDidLoad() {
